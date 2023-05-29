9b2e1874c1dfa0bd0914db406e17d180c2a3bef7
AndreyBassani2004 <83016044+AndreyBassani2004@users.noreply.github.com>
2023-05-29 12:55:18 -0300

    Correção 2905231255.

--- projeto-spring-data-aula/src/main/java/projeto/spring/data/aula/model/UsuarioSpringData.java
+++ projeto-spring-data-aula/src/main/java/projeto/spring/data/aula/model/UsuarioSpringData.java
@@ -1,32 +1,16 @@
 package projeto.spring.data.aula.model;
 
-import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-import javax.persistence.Transient;
-import javax.persistence.UniqueConstraint;
-import javax.persistence.Version;
 
-import org.hibernate.annotations.NaturalId;
+@Entity
+public class UsuarioSpringData {
 
-//@Entity
-//@Table(name = "usuariobanco", uniqueConstraints
-//			= {@UniqueConstraint(columnNames={"login", "senha", "cpf"})})
-
-//@SequenceGenerator(initialValue = 1, name = "usuario_sequence")
-//public class UsuarioSpringData {
-/*
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="usuario_sequence")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 
 	private String login;
@@ -37,38 +21,7 @@ import org.hibernate.annotations.NaturalId;
 
 	private String email;
 	
-	@Version
-	@Column(name="OPTLOCK")
-	private int version;
-	
-	@NaturalId
-	@Column(name = "cpg_pessoa", unique = true, updatable = false)
-	private String cpf;
-
-
-    @Lob
-	private byte[] foto;
-    
-    
-	
 	private int idade;
-	
-	@Transient
-	private String nomeAuxilixar;
-	
-	
-	
-	@OneToMany(mappedBy = "usuarioSpringData", 
-			orphanRemoval = true, fetch = FetchType.EAGER)
-	private List<Telefone> telefones;
-	
-	public void setTelefones(List<Telefone> telefones) {
-		this.telefones = telefones;
-	}
-	
-	public List<Telefone> getTelefones() {
-		return telefones;
-	}
 
 	
 	public Long getId() {
@@ -118,5 +71,5 @@ import org.hibernate.annotations.NaturalId;
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
-*/
-//}
+
+}
--- projeto-spring-data-aula/src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
+++ projeto-spring-data-aula/src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
@@ -1,19 +1,12 @@
 package projeto.spring.data.aula;
 
-import java.util.List;
-import java.util.Optional;
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-//import projeto.spring.data.aula.dao.InterfaceSpringDataUser;
-//import projeto.spring.data.aula.dao.InterfaceTelefone;
-//import projeto.spring.data.aula.model.Telefone;
-//import projeto.spring.data.aula.model.UsuarioSpringData;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:META-INF/spring-config.xml" })
