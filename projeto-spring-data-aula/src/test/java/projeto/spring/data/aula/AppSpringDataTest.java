4f5bd2eb5c88ff83912115b80222bb4ba43a1681
AndreyBassani2004 <83016044+AndreyBassani2004@users.noreply.github.com>
2023-05-30 13:36:39 -0300

    Realizando cadastro no banco de dados.

--- projeto-spring-data-aula/src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
+++ projeto-spring-data-aula/src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import projeto.spring.data.aula.dao.IntefaceSpringDataUser;
+import projeto.spring.data.aula.model.UsuarioSpringData;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,9 +19,23 @@ public class AppSpringDataTest {
 	@Autowired
 	private IntefaceSpringDataUser interfaceStprinDataUser;
 	
+	@Test
+	public void testInsert() {
+		
+		UsuarioSpringData usuarioSpringData =  new UsuarioSpringData();
+		
+		usuarioSpringData.setEmail("asbc@email.com");
+		usuarioSpringData.setNome("asbc def");
+		usuarioSpringData.setLogin("asbc");
+		usuarioSpringData.setSenha("1234");
+		usuarioSpringData.setIdade(27);
+		
+		interfaceStprinDataUser.save(usuarioSpringData);
+		System.out.println("Cadastrado com sucesso!");
+	}
 
 	@Test
-	public void testeInsert() {
+	public void testConsulta() {
 		System.out.println("Conectado com Spring! ");
 	}
 
