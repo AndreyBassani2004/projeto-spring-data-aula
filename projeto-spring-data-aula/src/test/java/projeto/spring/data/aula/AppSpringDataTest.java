31c8a284729eb5cc18a57cebfe090220db20a685
AndreyBassani2004 <83016044+AndreyBassani2004@users.noreply.github.com>
2023-05-30 08:53:44 -0300

    Criando nosso DAO Repository.

--- projeto-spring-data-aula/src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
+++ projeto-spring-data-aula/src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
@@ -4,22 +4,24 @@ package projeto.spring.data.aula;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import projeto.spring.data.aula.dao.IntefaceSpringDataUser;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:META-INF/spring-config.xml" })
 public class AppSpringDataTest {
 
-	
+	@Autowired
+	private IntefaceSpringDataUser interfaceStprinDataUser;
 	
 
 	@Test
 	public void testeInsert() {
-
-		System.out.println("conectado! ");
-
+		System.out.println("Conectado com Spring! ");
 	}
 
 	
